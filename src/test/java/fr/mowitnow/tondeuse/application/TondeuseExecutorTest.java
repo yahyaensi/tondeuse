66cc9b2dcec02b2c35092c26cbee6674b92ffc03
Yahya <yahya.romdhane.ensi@gmail.com>
2019-06-16 23:18:06 +0200

    implement failing tests

---
+++ src/test/java/fr/mowitnow/tondeuse/application/TondeuseExecutorTest.java
@@ -0,0 +1,42 @@
+package fr.mowitnow.tondeuse.application;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TondeuseExecutorTest {
+	
+	private ClassLoader loader = ClassLoader.getSystemClassLoader();
+	
+	
+	@Before
+	public void setup() throws IOException, URISyntaxException {
+		
+	}
+	
+	
+	@Test
+	public void rotate_tondeuse_to_left() {
+		fail("fail me");
+	}
+	
+	@Test
+	public void rotate_tondeuse_to_rigt() {
+		fail("fail me");
+	}
+	
+	@Test
+	public void move_tondeuse_forward() {
+		fail("fail me");
+	}
+	
+	@Test
+	public void move_tondeuse_to_final_position() {
+		fail("fail me");
+	}
+
+}
